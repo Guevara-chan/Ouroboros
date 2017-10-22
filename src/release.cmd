e9e570217108ddaff1e919ed0b89f804c741d8cb
Victoria Guevara <Guevara-chan@mail.ru>
2017-10-22 20:33:59 +0300

    Add files via upload

---
+++ src/release.cmd
@@ -0,0 +1,12 @@
+﻿setlocal
+chcp 65001
+set tmp_exe=Θuroboros.exe
+
+if exist %tmp_exe% ( del %tmp_exe% )
+C:/Utils/[dot]Net/Boo/bin/booc.exe -debug- Θuroboros.boo
+
+if exist %tmp_exe% (
+C:/Utils/Misc/ResHacker/ResourceHacker.exe -open %tmp_exe% -save %tmp_exe% -action addoverwrite -resource icon.ico -mask ICONGROUP, MAINICON, 0
+C:/Utils/[dot]Net/Extra/ILMerge/ilmerge.exe %tmp_exe% C:/Utils/[dot]Net/Boo/bin/Boo.Lang.dll /t:winexe /ndebug /out:../「Θuroboros」.exe
+del %tmp_exe%
+)
