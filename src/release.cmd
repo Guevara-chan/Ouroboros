1f1120d5e53e4189113874f8724f115349d8ae82
Victoria Guevara <Guevara-chan@mail.ru>
2017-11-02 11:25:27 +0300

    Add files via upload

--- src/release.cmd
+++ src/release.cmd
@@ -1,12 +1,14 @@
-﻿setlocal
+﻿REM --Setup util_root for actual path.
 chcp 65001
+setlocal
 set tmp_exe=Θuroboros.exe
+set util_root=C:/Utils
 
 if exist %tmp_exe% ( del %tmp_exe% )
-C:/Utils/[dot]Net/Boo/bin/booc.exe -debug- Θuroboros.boo
+%util_root%/[dot]Net/Boo/bin/booc.exe -debug- Θuroboros.boo
 
 if exist %tmp_exe% (
-C:/Utils/Misc/ResHacker/ResourceHacker.exe -open %tmp_exe% -save %tmp_exe% -action addoverwrite -resource icon.ico -mask ICONGROUP, MAINICON, 0
-C:/Utils/[dot]Net/Extra/ILMerge/ilmerge.exe %tmp_exe% C:/Utils/[dot]Net/Boo/bin/Boo.Lang.dll /t:winexe /ndebug /out:../「Θuroboros」.exe
+%util_root%/Misc/ResHacker/ResourceHacker.exe -open %tmp_exe% -save %tmp_exe% -action addoverwrite -resource icon.ico -mask ICONGROUP, MAINICON, 0
+%util_root%/[dot]Net/Extra/ILMerge/ilmerge.exe %tmp_exe% C:/Utils/[dot]Net/Boo/bin/Boo.Lang.dll /t:winexe /ndebug /out:../「Θuroboros」.exe
 del %tmp_exe%
 )
